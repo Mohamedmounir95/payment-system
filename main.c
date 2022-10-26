eb56f32784089bc3318b026f77e8fe2b806de728
mohamed mounir <116754661+Mohamedmounir95@users.noreply.github.com>
2022-10-26 22:16:37 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#pragma warning(disable : 4996)
+#include<stdio.h>
+//including header file
+#include "../payment app/Application/app.h"
+
+int main() {
+	//calling the appstart function
+	appstart();
+}
